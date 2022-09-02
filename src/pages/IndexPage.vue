64427d0e3bf58c990887839aa6adc68fef6694a8
KaiqueDiniz <kaiquesolaliendres07@gmail.com>
2022-09-02 09:25:04 -0400

    2 files modified

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,59 +1,43 @@
 <template>
-  <q-page class="column flex-center">
-    <q-knob
-      v-model="count"
-      :min="min"
-      :max="max"
-      size="80px"
-      show-value
-      :thickness="0.13"
-      color="primary"
-      track-color="dark"
-    >
-      <q-avatar size="75px">
-        <img alt="Quasar logo" src="~assets/quasar-logo-inner.svg" />
-      </q-avatar>
-    </q-knob>
-
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 140px"
-    />
-
-    <div class="q-mt-xl">
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="-"
-        :disable="count === min"
-        @click="count--"
-      />
-
-      <span class="q-mx-md text-bold">{{ count }}</span>
-
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="+"
-        :disable="count === max"
-        @click="count++"
-      />
-    </div>
-
-    <div class="q-mt-md" style="width: 200px">
-      <q-slider v-model="count" :min="min" :max="max" />
+  <q-page>
+    <div class="flex flex-center calculator column">
+      <h4>Calculadora</h4>
+      <q-input v-model="firstNumber" label="Adicione um número"></q-input>
+      <q-input v-model="secondNumber" label="Adicione outro número"></q-input>
+      <q-btn class="button" @click="sum()" label="Somar"></q-btn>
+      <q-btn class="button" @click="multiply()" label="Multiplicar"></q-btn>
+      <q-btn class="button" @click="subtract()" label="Subtrair"></q-btn>
+      <q-btn class="button" @click="divide()" label="Dividir"></q-btn>
+      <span>O resultado é {{ result }}</span>
     </div>
   </q-page>
 </template>
 
-<script setup>
+<script>
 import { ref } from 'vue';
+export default {
+  setup() {
+    const firstNumber = ref();
+    const secondNumber = ref();
+    const result = ref(Number());
 
-const count = ref(0);
-
-const min = -5;
-const max = 5;
+    return {
+      firstNumber,
+      secondNumber,
+      result,
+      sum() {
+        result.value = Number(firstNumber.value) + Number(secondNumber.value);
+      },
+      multiply() {
+        result.value = Number(firstNumber.value) * Number(secondNumber.value);
+      },
+      subtract() {
+        result.value = Number(firstNumber.value) - Number(secondNumber.value);
+      },
+      divide() {
+        result.value = Number(firstNumber.value) / Number(secondNumber.value);
+      },
+    };
+  },
+};
 </script>
